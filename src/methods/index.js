8e9ba02fc626fa740a6fe731dcfc5e1bd61d1f07
Abdi Mo <65041807+abdimohamud@users.noreply.github.com>
2021-06-08 19:05:50 -0700

    Update index.js

--- src/methods/index.js
+++ src/methods/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 export const fetchUsers = (token) => {
   return axios.get(
     `https://onebox.demo.aserto.com/api/v1/dir/users?page.size=10&page.token=${
